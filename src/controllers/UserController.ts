95d6bc0e6ff89cd155baf6fa18685a57518d3781
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2021-07-26 22:03:48 -0300

    update user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { UserService } from "../services/UserService";
 
 class UserController {
-    async create(request: Request, response: Response) {
+    async create(request: Request, response: Response): Promise<Response> {
         const { email } = request.body;
 
         const userService = new UserService();
